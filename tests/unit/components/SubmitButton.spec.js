f580053977c3c39532a6fb619a45c0dca41e8e26
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-12-18 17:16:17 +0100

    test() New component test

---
+++ tests/unit/components/SubmitButton.spec.js
@@ -0,0 +1,26 @@
+/*global describe, it */
+
+import { mount, shallowMount } from '@vue/test-utils'
+import { expect } from 'chai'
+import Vue from 'vue'
+import SubmitButton from '@/components/SubmitButton.vue'
+
+describe('SubmitButton.vue', () => {
+  it('displays a non authorized message', () => {
+    const msg = 'submit'
+    const wrapper = shallowMount(SubmitButton, {
+      propsData: {msg}
+    })
+    expect(wrapper.find('span').text()).to.be.equal('Not Authorized')
+    expect(wrapper.find('button').text()).to.be.equal(msg)
+  })
+  it('displays an admin privilege message', () => {
+    const msg = 'submit'
+    const wrapper = shallowMount(SubmitButton, {
+      propsData: {msg, isAdmin: true}
+    })
+    expect(wrapper.find('span').text()).to.be.equal('Admin Privileges')
+    expect(wrapper.find('button').text()).to.be.equal(msg)
+  })
+})
+
