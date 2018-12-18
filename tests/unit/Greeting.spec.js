f5e860e6615ad96305d66da39a8c2681efb1ba59
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-12-18 12:42:22 +0100

    test() mount vs shallowMount

--- tests/unit/Greeting.spec.js
+++ tests/unit/Greeting.spec.js
@@ -1,12 +1,35 @@
 /*global describe, it */
 
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import Greeting from '@/components/Greeting.vue'
 import { expect } from 'chai'
+import Vue from 'vue'
 
 describe('Greeting.vue', () => {
   it('renders a greeting', () => {
     const wrapperGreetingComponents = mount(Greeting)
     expect(wrapperGreetingComponents.text().includes('Vue and TDD')).to.be.equal(true)
+  })
+  describe('mount and shallowMount', () => {
+    const Child = Vue.component('Child', {
+      name: 'Child',
+      template: '<div>Child component</div>'
+    })
+    const Parent = Vue.component('Parent', {
+      name: 'Parent',
+      template: '<div><Child /></div>'
+    })
+    it('Should shallowMount the components', () => {
+      let shallowWrapper = shallowMount(Child)
+      expect(shallowWrapper.text()).to.be.equal('Child component')
+      shallowWrapper = shallowMount(Parent)
+      expect(shallowWrapper.text()).to.not.be.equal('<child-stub></child-stub>')
+    })
+    it('Should mount the components', () => {
+      let wrapper = mount(Child)
+      expect(wrapper.text()).to.be.equal('Child component')
+      wrapper = shallowMount(Parent)
+      expect(wrapper.text()).to.not.be.equal('Child component')
+    })
   })
 })
