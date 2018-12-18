a0c91c73033df86cbc291849981afa45cf4f6e6e
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-12-18 17:35:50 +0100

    feat() New component for numbers

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,28 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <Greeting/>
     <SubmitButton msg="Message from App.vue" isAdmin="true"/>
+    <NumberRenderer even="false"/>
   </div>
 </template>
 
 <script>
-import Greeting from './components/Greeting.vue'
-import SubmitButton from './components/SubmitButton.vue'
+import Greeting from "./components/Greeting.vue";
+import SubmitButton from "./components/SubmitButton.vue";
+import NumberRenderer from "./components/NumberRenderer.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Greeting,
-    SubmitButton
+    SubmitButton,
+    NumberRenderer
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/NumberRenderer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>{{numbers}}</div>
+</template>
+
+<script>
+export default {
+  name: "NumberRenderer",
+  props: {
+    even: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    getNumbersByKind: isEven =>
+      isEven === true
+        ? [2, 4, 6, 8].join(", ")
+        : [1, 3, 5, 7, 9].join(", ")
+  },
+  computed: {
+    numbers() {
+      return this.getNumbersByKind(this.even);
+    }
+  }
+};
+</script>
+
