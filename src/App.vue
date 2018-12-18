a8541e477bfaa571ffc17f1692d94f5072d9a6c4
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-12-18 12:19:55 +0100

    Include in App.vue the component Greeting

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Greeting/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Greeting from './components/Greeting.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Greeting
   }
 }
 </script>
